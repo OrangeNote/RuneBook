87a0f6dac37f115866cfeaedcf3e52dae86b81fa
OrangeNote <orangenote.mail@gmail.com>
2018-09-14 14:56:26 +0200

    fix tooltips pos by setting window minwidth to ui breakpoint

--- src/main.js
+++ src/main.js
@@ -25,9 +25,9 @@ let latestv = null;
 
 function createWindow() {
 
-    let width = 760;
+    let width = 768;
     let height = 760;
-    let minWidth = 760;
+    let minWidth = 768;
     let minHeight = 560;
 
     let mainWindowState = windowStateKeeper({
